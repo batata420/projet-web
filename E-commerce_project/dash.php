33ddb1e03b87badd4fa2edb76f3ce684e7aa3a18
mohamedalisaidg5 <115824639+batata420@users.noreply.github.com>
2023-03-08 15:45:00 +0100

    Update dash.php

--- E-commerce_project/dash.php
+++ E-commerce_project/dash.php
@@ -75,7 +75,7 @@
 
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
           <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            <h1 class="h2">mafamachay lina hhh</h1>
+            <h1 class="h2">bienvenue chers Admin </h1>
          
           </div>
            
